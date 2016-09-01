8c8cd1f990ac6d149de25d94d7e478c56a6be03a
Ronald Portier <ronald@therp.nl>
2016-09-01 15:31:40 +0200

    [FIX] Add handling of changed relation type conditions.

--- partner_relations/models/res_partner_relation_type.py
+++ partner_relations/models/res_partner_relation_type.py
@@ -3,6 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 """Define the type of relations that can exist between partners."""
 from openerp import _, api, fields, models
+from openerp.exceptions import ValidationError
+
+
+HANDLE_INVALID_ONCHANGE = [
+    ('restrict',
+     _('Do not allow change that will result in invalid relations')),
+    ('ignore',
+     _('Allow existing relations that do not fit changed conditions')),
+    ('end',
+     _('End relations per today, if they do not fit changed conditions')),
+    ('delete',
+     _('Delete relations that do not fit changed conditions')),
+]
 
 
 class ResPartnerRelationType(models.Model):
@@ -45,10 +58,20 @@ class ResPartnerRelationType(models.Model):
     )
     is_symmetric = fields.Boolean(
         string='Symmetric',
-        old_name='symmetric',
         help="This relation is the same from right to left as from left to"
              " right",
         default=False,
+    )
+    handle_invalid_onchange = fields.Selection(
+        selection=HANDLE_INVALID_ONCHANGE,
+        string='Invalid relation handling',
+        required=True,
+        default='restrict',
+        help="When adding relations criteria like partner type and category"
+             " are checked.\n"
+             "However when you change the criteria, there might be relations"
+             " that do not fit the new criteria.\n"
+             "Specify how this situation should be handled.",
     )
 
     @api.model
@@ -69,3 +92,101 @@ class ResPartnerRelationType(models.Model):
                 'contact_type_right': self.contact_type_left,
                 'partner_category_right': self.partner_category_left,
             })
+
+    @api.multi
+    def check_existing(self, vals):
+        """Check wether records exist that do not fit new criteria."""
+        relation_model = self.env['res.partner.relation']
+        for rec in self:
+            handling = (
+                'handle_invalid_onchange' in vals and
+                vals['handle_invalid_onchange'] or
+                self.handle_invalid_onchange
+            )
+            if handling == 'ignore':
+                continue
+            # only look at relations for this type
+            invalid_domain = [
+                ('type_id', '=', rec.id),
+            ]
+            contact_type_left = (
+                'contact_type_left' in vals and vals['contact_type_left'] or
+                False
+            )
+            if contact_type_left == 'c':
+                # Valid records are companies:
+                invalid_domain.append(
+                    ('left_partner_id.is_company', '=', False)
+                )
+            if contact_type_left == 'p':
+                # Valid records are persons:
+                invalid_domain.append(
+                    ('left_partner_id.is_company', '=', True)
+                )
+            contact_type_right = (
+                'contact_type_right' in vals and vals['contact_type_right'] or
+                False
+            )
+            if contact_type_right == 'c':
+                # Valid records are companies:
+                invalid_domain.append(
+                    ('right_partner_id.is_company', '=', False)
+                )
+            if contact_type_right == 'p':
+                # Valid records are persons:
+                invalid_domain.append(
+                    ('right_partner_id.is_company', '=', True)
+                )
+            partner_category_left = (
+                'partner_category_left' in vals and
+                vals['partner_category_left'] or
+                False
+            )
+            if partner_category_left:
+                # records that do not have the specified category are invalid:
+                invalid_domain.append(
+                    ('left_partner_id.category_id', 'not in',
+                     partner_category_left)
+                )
+            partner_category_right = (
+                'partner_category_right' in vals and
+                vals['partner_category_right'] or
+                False
+            )
+            if partner_category_right:
+                # records that do not have the specified category are invalid:
+                invalid_domain.append(
+                    ('right_partner_id.category_id', 'not in',
+                     partner_category_right)
+                )
+            invalid_relations = relation_model.with_context(
+                active_test=False
+            ).search(invalid_domain)
+            if invalid_relations:
+                if handling == 'restrict':
+                    raise ValidationError(
+                        _('There are already relations not satisfying the'
+                          'conditions for partner type or category.')
+                    )
+                elif handling == 'delete':
+                    invalid_relations.unlink()
+                else:
+                    # Delete future records, end other ones, ignore relations
+                    # already ended:
+                    cutoff_date = fields.Date.today()
+                    for relation in invalid_relations:
+                        if relation.date_start >= cutoff_date:
+                            relation.unlink()
+                        elif (not relation.date_end or
+                                relation.date_end > cutoff_date):
+                            relation.write({'date_end': cutoff_date})
+
+    @api.multi
+    def write(self, vals):
+        """Handle existing relations if conditions change."""
+        self.check_existing(vals)
+        return super(ResPartnerRelationType, self).write(vals)
+        vals = self._correct_vals(vals)
+        for rec in self:
+            rec.relation_id.write(vals)
+        return True
